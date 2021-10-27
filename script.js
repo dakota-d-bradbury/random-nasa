6801a84b379d4da89294036c6ea9ad99f66aad81
Dakota GA <dakotwageneralassembly@Dakotas-MacBook-Pro-2.local>
2021-10-26 19:18:40 -0500

    adds ajax functions

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-alert('JS is Loaded')
+//Constants
+
+const BASE_URL = 'https://api.nasa.gov/planetary/apod';
+const API_KEY = 'B77R3Ey6UIIGjN9eVG4gPCevUNCoIEbfVTccOXVX';
+
+//State Variables
+let apiData;
+//Cached Element References
+const $main = $('main');
+
+
+//Event Listeners
+
+
+//Functions
+function getData(){
+    // get api data and assign it to our apiData state variable
+    $.ajax(BASE_URL +'?api_key=' +API_KEY + '&count=6')
+    .then(function(data){
+        apiData = data;
+        WebGL2RenderingContext();
+    }, function(error) {
+
+    });
+}
+
+function render(){
+    //transfer our api data to the DOM
+}
